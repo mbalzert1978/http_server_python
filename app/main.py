43d810ac3e759dad54ec21a64dff245ec4f294aa
Markus Iorio <mbalzert@gmail.com>
2023-12-17 18:54:24 +0100

    pass 3nd stage

--- app/main.py
+++ app/main.py
@@ -1,15 +1,25 @@
 import socket
 
 
+def parse_request(request: bytes) -> str:
+    req_str = request.decode()
+    lines = req_str.split("\r\n")
+    print(lines)
+
+
 def main() -> None:
     server_socket = create_server()
     client_socket, _ = server_socket.accept()
-    _ = client_socket.recv(1024)  # 1024 is the buffer size
+    request = client_socket.recv(1024)
+    response = parse_request(request)
     response = "HTTP/1.1 200 OK\r\n\r\n"
     client_socket.sendall(response.encode())
     client_socket.close()
 
-def create_server(address: str = "127.0.0.1", port: int = 4221, *, reuse_port=True) -> socket.socket:
+
+def create_server(
+    address: str = "127.0.0.1", port: int = 4221, *, reuse_port=True
+) -> socket.socket:
     return socket.create_server(address=(address, port), reuse_port=reuse_port)
 
 
