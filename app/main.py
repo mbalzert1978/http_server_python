8fdcd67e619277ab500aabac9bb70553d59f7d21
Markus Iorio <mbalzert@gmail.com>
2024-01-08 12:57:10 +0100

    Respond with 404

--- app/main.py
+++ app/main.py
@@ -12,20 +12,23 @@ def main() -> None:
     con, _address = server_socket.accept()
     with contextlib.closing(con) as con:
         while data := con.recv(1024).decode(encoding="utf-8"):
+            print(data)
             lines = data.split("\r\n")
+            print(lines)
             _method, path, _http_version = operator.getitem(lines, 0).split()
+            print(_method, path, _http_version)
             if path == "/":
-                con.sendall(ok_200().encode())
+                con.sendall(ok_200())
             else:
-                con.sendall(not_found_404().encode())
+                con.sendall(not_found_404())
 
 
-def ok_200() -> str:
-    return "HTTP/1.1 200 OK\r\n\r\n"
+def ok_200() -> bytes:
+    return "HTTP/1.1 200 OK\r\n\r\n".encode()
 
 
-def not_found_404() -> str:
-    return "HTTP/1.1 404 NOT FOUND\r\n\r"
+def not_found_404() -> bytes:
+    return "HTTP/1.1 404 NOT FOUND\r\n\r".encode()
 
 
 def get_listener(
